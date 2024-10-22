5bfb668035202800afd9cf36168d1088b6c50212
1812 <francogadea1812@gmail.com>
2024-10-22 16:58:49 -0300

    deploy on netlify try 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
-  output: 'hybrid'
+  output: 'hybrid',
+
+  adapter: netlify()
+
 });
